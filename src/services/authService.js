cec21c05887179d187b42df5cc9499d85ef52661
nel-thedev <nelelias@gmail.com>
2023-05-24 15:51:21 -0400

    Add commented cors config

--- src/services/authService.js
+++ src/services/authService.js
@@ -12,6 +12,18 @@ export const get = (route) => {
 export const post = (route, body) => {
   let token = localStorage.getItem('authToken');
 
+  // const api = axios.create({ baseUrl: baseUrl });
+
+  // api.interceptors.request.use(async (config) => {
+  //   if (token) {
+  //     config.headers.Authorization = `Bearer ${token}`;
+
+  //     config.headers['Access-Control-Allow-Origin'] = '*';
+  //   }
+  //   return config;
+  // });
+
+  // return api.post(baseUrl + route, body);
   return axios.post(baseUrl + route, body, {
     headers: { Authorization: `Bearer ${token}` },
   });
