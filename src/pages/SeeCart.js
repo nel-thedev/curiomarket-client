f5d98ee76295ee795bf0c549c43603d4a3eac553
nel-thedev <nelelias@gmail.com>
2023-05-26 09:03:51 -0400

    Delete middle page between cart and checkout

--- src/pages/SeeCart.js
+++ src/pages/SeeCart.js
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { CartContext } from '../context/cart';
 import ItemCard from '../components/ItemCard';
 import { Link } from 'react-router-dom';
+import { post } from '../services/authService';
 
 const SeeCart = () => {
   const { cart, clearCart } = useContext(CartContext);
+  const [message, setMessage] = useState('');
 
   const storedCart = localStorage.getItem('shoppingCart');
 
@@ -13,6 +15,35 @@ const SeeCart = () => {
       // acc + curr.value
       return acc + Number(curr.value);
     }, 0) || 0;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const storedCart = localStorage.getItem('shoppingCart');
+
+    const cart = JSON.parse(storedCart);
+
+    post('/checkout/create-checkout-session', cart)
+      .then((response) => {
+        window.location = response.data.url;
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+
+    if (query.get('success')) {
+      setMessage('Order placed! You will receive an email confirmation.');
+    }
+
+    if (query.get('canceled')) {
+      setMessage(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
 
   return (
     <div className="row p-5">
@@ -38,16 +69,19 @@ const SeeCart = () => {
           )}
         </div>
       </div>
-      <div className="col-md-4 col-xl-2 bg-secondary bg-opacity-10 py-3">
+      <div className="col-md-4 col-xl-2 bg-secondary bg-opacity-10 py-3 rounded-3">
+        {message ? <p>{message}</p> : <></>}
         <h3>Checkout</h3>
         <div>
           <p className="mt-4">Total:</p>
           <p>$ {subtotal}</p>
         </div>
 
-        <Link to={'/checkout'}>
-          <button className="btn btn-primary">Checkout</button>
-        </Link>
+        {/* <Link to={'/checkout'}> */}
+        <button className="btn btn-primary" onClick={handleSubmit}>
+          Checkout
+        </button>
+        {/* </Link> */}
       </div>
     </div>
   );
